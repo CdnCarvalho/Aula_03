c61409f05d6bde0476f2af619e83f46033973f69
CdnCarvalho <claudenirfer@gmail.com>
2025-09-05 11:51:14 -0300

    Aula 3 - Estrutura if else elif

---
+++ atividade_01.py
@@ -0,0 +1,10 @@
+# Verificar Número e verificar se é positivo ou negativo
+
+num = float(input('Informe um número: '))
+
+if num < 0:
+    print('Negativo')
+if num > 0:
+    print('Positivo')
+else:
+    print('Zero é Neutro')
---
+++ exemplo_01.py
@@ -0,0 +1,6 @@
+idade = int(input('Informe a sua idade: '))
+
+if idade >= 18:
+    print('Você é maior!')
+else:
+    print('Você é menor de idade! ')
